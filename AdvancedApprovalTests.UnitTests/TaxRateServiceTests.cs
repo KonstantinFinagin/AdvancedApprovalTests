730f2f5f2a19213d171fd010199a454b66165d41
kfinagin <konstantin.finagin@dataart.com>
2021-09-05 17:14:10 +0300

    part1 - Switching to approval tests, red

--- AdvancedApprovalTests.UnitTests/TaxRateServiceTests.cs
+++ AdvancedApprovalTests.UnitTests/TaxRateServiceTests.cs
@@ -1,10 +1,21 @@
 ﻿using System.Linq;
 using AdvancedApprovalTests.BL.Services;
 using AdvancedApprovalTests.Domain;
+using ApprovalTests.Namers;
+using ApprovalTests.Reporters;
+using Newtonsoft.Json;
 using Xunit;
 
 namespace AdvancedApprovalTests.UnitTests
 {
+#if DEBUG
+    // DIFF REPORTER is used to approve test results on a developer's machine
+    [UseReporter(typeof(DiffReporter))]
+#else
+    // QUIET REPORTER is used when we run tests in CI/CD pipeline 
+    [UseReporter(typeof(QuietReporter))]
+#endif
+    [UseApprovalSubdirectory("Results")]
     public class TaxRateServiceTest
     {
         private readonly TaxRateService _service;
@@ -17,41 +28,19 @@ namespace AdvancedApprovalTests.UnitTests
         [Fact]
         public void ShouldProvideCorrectRates_ForFlatTaxType()
         {
-            var response = _service.GetTaxRates(ETaxType.Flat).ToList();
+            var response = _service.GetTaxRates(ETaxType.Flat);
+            var jsonResponse = JsonConvert.SerializeObject(response, Formatting.Indented);
 
-            Assert.Single(response);
-
-            Assert.Equal(0.13m, response[0].Rate);
-            Assert.Null(response[0].MinAmount);
-            Assert.Null(response[0].MaxAmount);
+            ApprovalTests.Approvals.Verify(jsonResponse);
         }
 
         [Fact]
         public void ShouldProvideCorrectRates_ForProgressiveTaxType()
         {
-            var response = _service.GetTaxRates(ETaxType.Progressive).ToList();
+            var response = _service.GetTaxRates(ETaxType.Progressive);
+            var jsonResponse = JsonConvert.SerializeObject(response, Formatting.Indented);
 
-            Assert.Equal(5, response.Count);
-
-            Assert.Equal(0.00m, response[0].Rate);
-            Assert.Equal(0,     response[0].MinAmount);
-            Assert.Equal(1000,  response[0].MaxAmount);
-
-            Assert.Equal(0.05m, response[1].Rate);
-            Assert.Equal(1001,  response[1].MinAmount);
-            Assert.Equal(5000,  response[1].MaxAmount);
-
-            Assert.Equal(0.10m, response[2].Rate);
-            Assert.Equal(5001,  response[2].MinAmount);
-            Assert.Equal(10000, response[2].MaxAmount);
-
-            Assert.Equal(0.20m,   response[3].Rate);
-            Assert.Equal(10_001,  response[3].MinAmount);
-            Assert.Equal(100_000, response[3].MaxAmount);
-
-            Assert.Equal(0.35m,   response[4].Rate);
-            Assert.Equal(100_001, response[4].MinAmount);
-            Assert.Null(response[4].MaxAmount);
+            ApprovalTests.Approvals.Verify(jsonResponse);
         }
     }
 }
