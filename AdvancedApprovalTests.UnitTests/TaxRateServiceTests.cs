527dc63bd297c0b2bb8a45faf752b0c94122e762
kfinagin <konstantin.finagin@dataart.com>
2021-09-05 16:38:36 +0300

    part1 - First red test

---
+++ AdvancedApprovalTests.UnitTests/TaxRateServiceTests.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Linq;
+using AdvancedApprovalTests.BL.Services;
+using AdvancedApprovalTests.Domain;
+using Xunit;
+
+namespace AdvancedApprovalTests.UnitTests
+{
+    public class TaxRateServiceTest
+    {
+        private readonly TaxRateService _service;
+
+        public TaxRateServiceTest()
+        {
+            _service = new TaxRateService();
+        }
+
+        [Fact]
+        public void ShouldProvideCorrectRates_ForFlatTaxType()
+        {
+            var response = _service.GetTaxRates(ETaxType.Flat).ToList();
+
+            Assert.Single((IEnumerable)response);
+
+            Assert.Equal(0.13m, response[0].Rate);
+            Assert.Null(response[0].MinAmount);
+            Assert.Null(response[0].MinAmount);
+        }
+    }
+}
