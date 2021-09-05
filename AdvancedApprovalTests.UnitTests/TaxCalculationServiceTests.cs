7a2309d47e4af0dbc84ec262037399d340873e0e
kfinagin <konstantin.finagin@dataart.com>
2021-09-06 01:25:46 +0300

    part1 - progressive tests green

--- AdvancedApprovalTests.UnitTests/TaxCalculationServiceTests.cs
+++ AdvancedApprovalTests.UnitTests/TaxCalculationServiceTests.cs
@@ -44,12 +44,31 @@ namespace AdvancedApprovalTests.UnitTests
 
             _rateServiceMock.Setup(r => r.GetTaxRates(ETaxType.Flat)).Returns(new List<TaxRate>()
             {
-                new TaxRate() { Id = 150, MinAmount = 0, MaxAmount = decimal.MaxValue, Rate = 0.10m }
+                new TaxRate() { Id = 1, MinAmount = 0, MaxAmount = decimal.MaxValue, Rate = 0.10m }
             });
 
             _incomeRepositoryMock.Setup(i => i.GetFiltered(2020)).ReturnsAsync(testRecords);
             
             var response = _service.CalculateYearlyTaxAsync(new List<long>() { 1 }, 2020, ETaxType.Flat);
+
+            ApprovalTests.Approvals.Verify(JsonConvert.SerializeObject(response, Formatting.Indented));
+        }
+
+        [Fact]
+        public async Task ShouldCalculateProgressiveTaxCorrectly()
+        {
+            var testRecords = TestEmployeeIncomeRecordFactory.GetTestIncomeRecords();
+            _rateServiceMock.Setup(r => r.GetTaxRates(ETaxType.Progressive)).Returns(new List<TaxRate>()
+            {
+                new TaxRate() { Id = 1, MinAmount = 0, MaxAmount = 1000, Rate = 0.0m },
+                new TaxRate() { Id = 2, MinAmount = 1001, MaxAmount = 5000, Rate = 0.10m },
+                new TaxRate() { Id = 3, MinAmount = 5001, MaxAmount = 20000, Rate = 0.20m },
+                new TaxRate() { Id = 4, MinAmount = 20001, MaxAmount = decimal.MaxValue, Rate = 0.30m }
+            });
+
+            _incomeRepositoryMock.Setup(i => i.GetFiltered(2020)).ReturnsAsync(testRecords);
+
+            var response = _service.CalculateYearlyTaxAsync(new List<long>() { 1 }, 2020, ETaxType.Progressive);
 
             ApprovalTests.Approvals.Verify(JsonConvert.SerializeObject(response, Formatting.Indented));
         }
